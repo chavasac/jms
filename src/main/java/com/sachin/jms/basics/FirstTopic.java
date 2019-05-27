d826edc0f3362eab8d621e0effc7bef41736576a
chavasac <sachinchavan0213@gmail.com>
2019-05-28 00:30:54 +0530

    Added topic and multiple consumer example

--- src/main/java/com/sachin/jms/basics/FirstTopic.java
+++ src/main/java/com/sachin/jms/basics/FirstTopic.java
@@ -13,24 +13,26 @@ public class FirstTopic {
 
 	public static void main(String[] args) throws Exception {
 		InitialContext initialContext = new InitialContext();
-		ConnectionFactory cf = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
 		Topic topic = (Topic) initialContext.lookup("topic/myTopic");
 		
+		ConnectionFactory cf = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
 		Connection con = cf.createConnection();
-		Session session = con.createSession();
-		TextMessage textMessage = session.createTextMessage("I am the creator of this topic to which you are listening to");
 		
+		Session session = con.createSession();
 		MessageProducer producer = session.createProducer(topic);
-		producer.send(textMessage);
 		
 		MessageConsumer consumer1 = session.createConsumer(topic);
 		MessageConsumer consumer2 = session.createConsumer(topic);
 		
+		TextMessage textMessage = session.createTextMessage("I am the creator of this topic to which you are listening to");
+		
+		producer.send(textMessage);
+		
 		con.start();
 		
-		TextMessage messageReceived1 = (TextMessage) consumer1.receive(5000);
-		TextMessage messageReceived2 = (TextMessage) consumer2.receive(5000);
+		TextMessage messageReceived1 = (TextMessage) consumer1.receive(1000);
 		System.out.println("Message received by consumer 1 : " + messageReceived1.getText());
+		TextMessage messageReceived2 = (TextMessage) consumer2.receive(1000);
 		System.out.println("Message received by consumer 2 : " + messageReceived2.getText());
 		
 		session.close();
